f4489c81b911528630c5c040dae981a703cfd2e1
Kevin Chen <kchen@lcs.on.ca>
2026-04-15 21:26:28 -0400

    Initial Commit

---
+++ Economic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Economic
+//
+//  Created by Haowen Chen on 2026-04-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Economic/EconomicApp.swift
@@ -0,0 +1,17 @@
+//
+//  EconomicApp.swift
+//  Economic
+//
+//  Created by Haowen Chen on 2026-04-15.
+//
+
+import SwiftUI
+
+@main
+struct EconomicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
