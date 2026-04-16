0697f8a5e3beffeb85c6e841ff739c42388d885c
Kevin Chen <kchen@lcs.on.ca>
2026-04-16 02:03:03 -0400

    Tryed add a function that can display history

--- Economic/View/PageView.swift
+++ Economic/View/PageView.swift
@@ -5,3 +5,35 @@
 //  Created by Haowen Chen on 2026-04-16.
 //
 
+//import SwiftUI
+//
+//struct LandingView: View {
+//    var body: some View {
+//        TabView {
+//            PlayersListView()
+//                .tabItem {
+//                    Label(
+//                        "Players",
+//                        systemImage: "person.fill"
+//                    )
+//                }
+//            QuestionsListView()
+//                .tabItem {
+//                    Label(
+//                        "Questions",
+//                        systemImage: "rectangle.inset.filled"
+//                    )
+//                }
+//            
+//            PlayerView()
+//                .tabItem {
+//                    Image(systemName: "person.fill")
+//                    Text("My profile")
+//                }
+//        }
+//    }
+//}
+//
+//#Preview {
+//    LandingView()
+//}
